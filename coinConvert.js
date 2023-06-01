48f5cf5f377b021a7418c1bd996af9477ab83fb4
Sarah <90399406+sarahguerreiro@users.noreply.github.com>
2023-06-01 14:49:13 -0300

    mais exercicios

---
+++ coinConvert.js
@@ -0,0 +1,16 @@
+1 dólar = 3.25 soles
+1 dólar = 18 pesos mexicanos
+1 dólar = 660 pesos chilenos
+
+const coinConvert = (dollars) => {
+    // Atribuir o valor equivalente em soles
+  const soles = /* ??? */;
+  
+    // Atribuir o valor equivalente em pesos mexicanos
+  const pesosMexicanos = /* ??? */;
+  
+    // Atribuir o valor equivalente em pesos chilenos
+  const pesosChilenos = /* ??? */;
+  
+  return [soles, pesosMexicanos, pesosChilenos];
+};
